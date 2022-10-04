f7e2d55cf4ca7609ee23b2b43102d98f5b6dd92a
Nicholas Edwards <edwardsnjd@googlemail.com>
2022-10-04 22:43:11 +0800

    feat: Added LSP for ruby
    This is implemented using the SolarGraph LSP.

---
+++ ruby/README.md
@@ -0,0 +1,3 @@
+# Ruby LSP
+
+This provides the standard [`SolarGraph ruby lsp`](https://solargraph.org/) in a container.
---
+++ ruby/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# A shim to make starting the container easier
+
+exec docker run \
+  --interactive \
+  --rm \
+  --volume "$(pwd)":"$(pwd)" \
+  --workdir "$(pwd)" \
+  --init \
+  lsp-ruby \
+  "$@"
